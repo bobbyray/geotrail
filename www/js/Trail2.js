1a11868616d2e0a2141904a6859ed046dacd7e16
bobbyray <bobschomburg@gmail.com>
2016-11-05 13:57:46 -0700

    Fix license
    cordova-plugin-geolocation npm v1.0.0 was missing.

--- www/js/Trail2.js
+++ www/js/Trail2.js
@@ -42,7 +42,7 @@ wigo_ws_GeoPathMap.OfflineParams = function () {
 // Object for View present by page.
 function wigo_ws_View() {
     // Release buld for Google Play on 09/20/2016 16:03
-    var sVersion = "1.1.021  10/23/2016_1630"; // Constant string for App version.
+    var sVersion = "1.1.021  11/05/2016_1146"; // Constant string for App version.
 
     // ** Events fired by the view for controller to handle.
     // Note: Controller needs to set the onHandler function.
--- www/js/devicedetails.js
+++ www/js/devicedetails.js
@@ -15,8 +15,8 @@ https://github.com/bobbyray/MitLicense/releases/tag/v1.0
 // This devicedetails.js file has the same name for android and iPhone
 // builds, but the content is different.   
 function Wigo_Ws_getDeviceType() {
-    //var nDevice = app.deviceDetails.DeviceEnum().android;
+    var nDevice = app.deviceDetails.DeviceEnum().android;
     ////20161023 Undo iphone
-    var nDevice = app.deviceDetails.DeviceEnum().iPhone;
+    // var nDevice = app.deviceDetails.DeviceEnum().iPhone;
     return nDevice;
 }
